458685a48a769fe2f2ae2ea0d050dde8a7806b9d
ozlemakbaba <34067964+ozlemakbaba@users.noreply.github.com>
2017-11-28 18:00:57 +0200

    deneme

---
+++ ornek1/cmake-build-debug/CMakeFiles/ornek1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ornek1.dir/main.c.obj"
+  "ornek1.pdb"
+  "ornek1.exe"
+  "ornek1.exe.manifest"
+  "libornek1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ornek1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ornek1/cmake-build-debug/CMakeFiles/ornek1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ornek1.
+# This may be replaced when dependencies are built.
